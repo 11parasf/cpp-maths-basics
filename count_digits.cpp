d00e006ea945db9bcd4c60980fee5f6712499ffd
Priyanshu Poonia <11parasf@users.noreply.github.com>
2026-01-04 10:04:21 +0530

    Add Optimal Solution(count_digit) using Logarithm Function

--- count_digits.cpp
+++ count_digits.cpp
@@ -1,13 +1,8 @@
 #include <iostream>   // For input and output operations
 using namespace std;
 
-int main() {
-    int n;  // Variable to store the input number
-
-    // Ask the user to enter a number
-    cout << "Enter a number: ";
-    cin >> n;
-
+//Brute Force
+void countBydividing (int n) {
     int count = 0;   // To store the count of digits
     int temp = n;    // Temporary variable to manipulate the number
 
@@ -25,6 +20,25 @@ int main() {
 
     // Display the result
     cout << "Number of digits: " << count << endl;
+}
+
+//Optimal 
+int countDigit (int n) {
+    return log10(n) + 1;  //Time complexity 0(1) , Space Complexity 0(1) 
+}
+
+int main() {
+    int n;  // Variable to store the input number
+
+    // Ask the user to enter a number
+    cout << "Enter a number: ";
+    cin >> n;
+
+    //Brute Force Function
+    countBydividing(n);
+    
+    //Optimal 
+    std::cout << "Number of digits(using logarithmic function) : " << countDigit(n) << std::endl;
 
     return 0;  // Program ends successfully
 }
