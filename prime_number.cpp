56846d2f386fe06df494aa9b51cb0420231b75f9
Priyanshu Poonia <11parasf@users.noreply.github.com>
2026-01-06 00:47:04 +0530

    Add prime number problem with brute and optimal solutions

---
+++ prime_number.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int main()
+{
+    int num = 0;
+    std::cout << "Enter a number : ";
+    std::cin >> num;
+
+    // Brute Force TC - O(num)
+    int count = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+
+            count += 1;
+        }
+    }
+    if (count == 2)
+    {
+        std::cout << num << " is a prime number" << std::endl;
+    }
+    else
+    {
+        std::cout << num << " is not a prime number." << std::endl;
+    }
+
+    // Optimal Solution TC- O(sqrt(num))
+    count = 0;
+    for (int i = 1; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            count += 1;
+            if (num / i != i)
+            {
+                count += 1;
+            }
+            if (count > 2)
+            {
+                break;
+            }
+        }
+    }
+    if (count == 2)
+    {
+        std::cout << num << " is a prime number" << std::endl;
+    }
+    else
+    {
+        std::cout << num << " is not a prime number." << std::endl;
+    }
+
+    return 0;
+}
