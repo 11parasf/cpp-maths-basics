c49fe5cc97e3f8b7d386d86ba4f5975e0b75e2f1
Priyanshu Poonia <11parasf@users.noreply.github.com>
2026-01-09 22:59:50 +0530

    Add Optimal exponentiation of a number

--- power_exponentiation.cpp
+++ power_exponentiation.cpp
@@ -2,22 +2,52 @@
 #include <iostream>
 
 // Brute Force TC ~ O(N)
+// int main()
+// {
+//     int N;
+//     std::cout << "Enter a number : ";
+//     std::cin >> N;
+
+//     int exponentiation;
+//     std::cout << "Enter power : ";
+//     std::cin >> exponentiation;
+
+//     int power = 1;
+//     for (int i = 1; i <= exponentiation; i++)
+//     {
+//         power *= N;
+//     }
+//     std::cout << N << "^" << exponentiation << " = " << power << std::endl;
+
+//     return 0;
+// }
+
+// Optimal Solution TC ~ O(log₂(N))
+
+#include <iostream>
+
 int main()
 {
+    // Input from user
     int N;
     std::cout << "Enter a number : ";
     std::cin >> N;
-
-    int exponentiation;
+    int power;
     std::cout << "Enter power : ";
-    std::cin >> exponentiation;
+    std::cin >> power;
 
-    int power = 1;
-    for (int i = 1; i <= exponentiation; i++)
+    // Any number raised to power 0 = 1
+    int ans = 1;
+    while (power > 0)
     {
-        power *= N;
+        if (power % 2 != 0)
+        {
+            power -= 1;
+            ans *= N;
+        }
+        power /= 2;
+        N *= N;
     }
-    std::cout << N << "^" << exponentiation << " = " << power << std::endl;
-
+    std::cout << ans << std::endl;
     return 0;
 }
